b54717154960002d9ef14936529ca004c7dc1f88
GeorgeErimia <86672491+GeorgeErimia@users.noreply.github.com>
2023-06-16 13:48:52 +0300

    Added some fish in the water. TODO : add distinct fish (dolphins, other species)

--- OpenGLProject/main.cpp
+++ OpenGLProject/main.cpp
@@ -50,6 +50,10 @@ Material dullMaterial;
 Model mountains;
 Model ocean;
 Model submarine;
+Model fish;
+std::vector<Model> fishGroup1;
+std::vector<Model> fishGroup2;
+std::vector<Model> fishGroup3;
 
 DirectionalLight mainLight;
 
@@ -64,12 +68,22 @@ GLfloat lastTime = 0.0f;
 GLfloat submarineHeight = 0.0f;
 glm::vec3 lastSubmarinePos = glm::vec3(-7.0f, -2.2f, 0.0f);
 glm::vec3 submarinePos = glm::vec3(-7.0f, -2.2f, 0.0f);
-GLfloat submarineAngle = 0.0f;
 glm::vec3 submarineDirection(0.0f, 0.0f, 1.0f);
-
 GLfloat turnAngle = 0.0f;
+
+// Other object coordinates
 glm::vec3 waterPos = glm::vec3(0.0f, -2.0f, 0.0f);
 glm::vec3 mountainPos = glm::vec3(0.0f, 0.0f, 0.0f);
+
+// Fish Groups 
+std::vector<glm::vec3> fishGroup1_pos;
+std::vector<glm::vec3> fishGroup2_pos;
+std::vector<glm::vec3> fishGroup3_pos;
+GLfloat fishGroup1_angle = 0.0f;
+GLfloat fishGroup2_angle = 0.0f;
+GLfloat fishGroup3_angle = 0.0f;
+glm::vec3 fishGroup1_dir, fishGroup2_dir, fishGroup3_dir;
+
 
 // Vertex Shader
 static const char* vShader = "Shaders/shader.vert";
@@ -181,6 +195,145 @@ void setSubmarineKeyControl(bool* keys)
 		submarineDirection = glm::vec3(-sin(turnAngle), 0.0f, cos(turnAngle));
 	}
 }
+void InitFishes()
+{
+	// Fish Group 1
+	fishGroup1_pos.push_back(glm::vec3(-7.0f, -3.0f, -5.0f));
+	fishGroup1.push_back(fish);
+
+	fishGroup1_pos.push_back(glm::vec3(-8.0f, -3.0f, -5.0f));
+	fishGroup1.push_back(fish);
+
+	fishGroup1_pos.push_back(glm::vec3(-7.5f, -3.0f, -5.5f));
+	fishGroup1.push_back(fish);
+
+	fishGroup1_pos.push_back(glm::vec3(-7.7f, -3.4f, -4.5f));
+	fishGroup1.push_back(fish);
+
+	fishGroup1_pos.push_back(glm::vec3(-8.0f, -3.5f, -5.0f));
+	fishGroup1.push_back(fish);
+
+	// Fish Group 2
+	fishGroup2_pos.push_back(glm::vec3(-10.0f, -4.0f, -5.0f));
+	fishGroup2.push_back(fish);
+
+	fishGroup2_pos.push_back(glm::vec3(-9.5f, -3.5f, -4.0f));
+	fishGroup2.push_back(fish);
+
+	fishGroup2_pos.push_back(glm::vec3(-9.0f, -3.0f, -5.0f));
+	fishGroup2.push_back(fish);
+
+	fishGroup2_pos.push_back(glm::vec3(-9.5f, -3.5f, -6.0f));
+	fishGroup2.push_back(fish);
+
+	fishGroup2_pos.push_back(glm::vec3(-10.0f, -3.5f, -5.0f));
+	fishGroup2.push_back(fish);
+
+	// Fish Group 3
+	fishGroup3_pos.push_back(glm::vec3(-10.0f, -3.0f, 5.0f));
+	fishGroup3.push_back(fish);
+
+	fishGroup3_pos.push_back(glm::vec3(-9.5f, -3.5f, 4.0f));
+	fishGroup3.push_back(fish);
+
+	fishGroup3_pos.push_back(glm::vec3(-9.0f, -3.0f, 6.0f));
+	fishGroup3.push_back(fish);
+
+	fishGroup3_pos.push_back(glm::vec3(-9.5f, -3.5f, 3.0f));
+	fishGroup3.push_back(fish);
+
+	fishGroup3_pos.push_back(glm::vec3(-10.0f, -3.5f, 4.0f));
+	fishGroup3.push_back(fish);
+}
+
+void UpdateFishDirection()
+{
+	GLfloat velocity = 0.2f * deltaTime;
+
+	// Fish Group 1
+	fishGroup1_dir = glm::vec3(-sin(fishGroup1_angle), 0.0f, cos(fishGroup1_angle));
+	for (int i = 0; i < fishGroup1_pos.size(); i++)
+		fishGroup1_pos[i] -= velocity * fishGroup1_dir;
+
+	// Fish Group 2
+	fishGroup2_dir = glm::vec3(-sin(fishGroup2_angle), 0.0f, cos(fishGroup2_angle));
+	for (int i = 0; i < fishGroup2_pos.size(); i++)
+		fishGroup2_pos[i] -= velocity * fishGroup2_dir;
+
+	// Fish Group 3
+	for (int i = 0; i < fishGroup3_pos.size(); i++)
+	{
+		if (fishGroup3_pos.at(i).z + 1.0f > 9.0f && fishGroup3_dir.z < 2
+			|| fishGroup3_pos.at(i).z - 1.0f < 0.0f && fishGroup3_dir.z > 0)
+		{
+			if (fishGroup3_angle == 180)
+				fishGroup3_angle = 0;
+			else fishGroup3_angle = 180;
+		}
+	}
+	if(fishGroup3_angle == 180)
+		fishGroup3_dir = glm::vec3(0.0f, 0.0f, -1.0f);
+	else fishGroup3_dir = glm::vec3(0.0f, 0.0f, 1.0f);
+	for (int i = 0; i < fishGroup3_pos.size(); i++)
+		fishGroup3_pos[i] -= velocity * fishGroup3_dir;
+}
+
+void RenderFishes()
+{
+	glm::mat4 model;
+	fishGroup1_angle += 0.1f * deltaTime;
+	if (fishGroup1_angle > 360.0f)
+		fishGroup1_angle = 0.0f;
+
+	fishGroup2_angle -= 0.2f * deltaTime;
+	if (fishGroup2_angle < -360.0f)
+		fishGroup2_angle = 0.0f;
+
+	UpdateFishDirection();
+
+	// Render Fish Group 1
+	for (int i = 0; i < fishGroup1.size(); i++)
+	{
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, fishGroup1_pos.at(i));
+		model = glm::rotate(model, -fishGroup1_angle - 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::rotate(model, 80.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+		if (i % 2 == 0)
+		{
+			model = glm::scale(model, glm::vec3(0.02f, 0.02f, 0.02f));
+		}
+		else
+		{
+			model = glm::scale(model, glm::vec3(0.01f, 0.01f, 0.01f));
+		}
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		fishGroup1.at(i).RenderModel();
+	}
+
+	// Render Fish Group 2
+	for (int i = 0; i < fishGroup2.size(); i++)
+	{
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, fishGroup2_pos.at(i));
+		model = glm::rotate(model, -fishGroup2_angle - 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::rotate(model, 80.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.01f, 0.01f, 0.01f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		fishGroup2.at(i).RenderModel();
+	}
+
+	// Render Fish Group 3
+	for (int i = 0; i < fishGroup3.size(); i++)
+	{
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, fishGroup3_pos.at(i));
+		model = glm::rotate(model, -fishGroup3_angle - 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::rotate(model, 80.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.01f, 0.01f, 0.01f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		fishGroup2.at(i).RenderModel();
+	}
+}
 
 void RenderScene()
 {
@@ -213,6 +366,16 @@ void RenderScene()
 	waterTexture.UseTexture();
 	shinyMaterial.UseMaterial(uniformSpecularIntensity, uniformShininess);
 	submarine.RenderModel();
+
+	RenderFishes();
+
+	// Render fish
+	//model = glm::mat4(1.0f);
+	//model = glm::translate(model, submarinePos - glm::vec3(0.0f, 0.0f, -5.0f));
+	//model = glm::rotate(model, -90.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+	//model = glm::scale(model, glm::vec3(0.01f, 0.01f, 0.01f));
+	//glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+	//fish.RenderModel();
 
 	GLfloat velocity = 0.5f * deltaTime;
 }
@@ -308,8 +471,13 @@ int main()
 	ocean = Model();
 	ocean.LoadModel("Models/ocean.obj");
 
+	// Fish
+	fish = Model();
+	fish.LoadModel("Models/12265_Fish_v1_L2.obj");
 
-	// Light (MAIN)
+	InitFishes();
+
+	// Light
 	mainLight = DirectionalLight(2048, 2048,
 		0.7f, 0.7f, 0.8f,
 		0.1f, 0.9f,
@@ -327,34 +495,27 @@ int main()
 
 	skybox = Skybox(skyboxFaces);
 
-
-	//// Ids
-	//GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformEyePosition = 0,
-	//	uniformSpecularIntensity = 0, uniformShininess = 0;
 	glm::mat4 projection = glm::perspective(glm::radians(60.0f), (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 100.0f);
 
 	// Loop until window closed
 	while (!mainWindow.getShouldClose())
 	{
 		// Calculate delta time
-		GLfloat now = glfwGetTime(); // SDL_GetPerformanceCounter();
-		deltaTime = now - lastTime; // (now - lastTime)*1000/SDL_GetPerformanceFrequency();
+		GLfloat now = glfwGetTime();
+		deltaTime = now - lastTime; 
 		lastTime = now;
 
 		// Get + Handle User Input
 		glfwPollEvents();
 
-		// // Use key control when using old mouse control
-		//camera.keyControl(mainWindow.getsKeys(), deltaTime);
+		 // Use key control when not focusing on object
+		camera.keyControl(mainWindow.getsKeys(), deltaTime);
 
-		setSubmarineKeyControl(mainWindow.getsKeys());
-		camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange(), submarinePos, lastSubmarinePos);
+		//setSubmarineKeyControl(mainWindow.getsKeys());
+		//camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange(), submarinePos, lastSubmarinePos);
 
-		// // Old movement (not focused on object)
-		//camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
-
-
-		printf("%f %f %f\n", camera.getCameraPosition().x, camera.getCameraPosition().y, camera.getCameraPosition().z);
+		 // Old movement (not focused on object)
+		camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
 
 		DirectionalShadowMapPass(&mainLight);
 		RenderPass(camera.calculateViewMatrix(), projection);
